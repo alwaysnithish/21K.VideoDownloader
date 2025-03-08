eb586c880a48dbe5187218c736bc677523abbb69
alwaysnithish <inithish21k@gmail.com>
2025-03-08 17:52:27 +0530

    Update urls.py

--- ssvideodownloader/urls.py
+++ ssvideodownloader/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('aboutus', views.aboutus, name='home'),
+  
     path('privacypolicy/',views.privacypolicy,name='privacypolicy'),
     path('termsandconditions/',views.termsandconditions,name='termsandconditions'),
 
